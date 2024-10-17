223ca606d418d71fc7eab085dd29c6a189e02a4c
Nicolas <nicollasxpnc@gmail.com>
2024-10-17 16:26:07 -0300

    feat: tela de dados do motorista

---
+++ Controllers/EditarMotoristas.cs
@@ -0,0 +1,17 @@
+﻿using Acesv2.Models;
+using Acesvv.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+    public class EditarMotoristas : Controller
+    {
+        // GET: Transportadores/EditarMotorista
+        public IActionResult TelaDados()
+        {
+            // Renderiza a view da página localizada em Views/EditarMotorista/TelaDados.cshtml
+            return View();
+        }
+    }
+
+
