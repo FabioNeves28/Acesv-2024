6a422faa4e51500d9d50eba5e96ec47cc4a4824c
Fábio Neves <fneves@iportsolutions.com.br>
2024-10-28 16:23:31 -0300

    fix: pdf financeiro

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,7 +5,23 @@ document.getElementById('btnDownload').addEventListener('click', function () {
     var fontSize = 10;
     var maxTableWidth = 700;
     var table = document.getElementById('tabela');
-    var tableString = doc.autoTableHtmlToJson(table);
+    var tableClone = table.cloneNode(true);
+
+    var headerRow = tableClone.querySelector('thead tr');
+    var editHeader = headerRow.querySelector('th:nth-child(2)');
+    if (editHeader) {
+        headerRow.removeChild(editHeader);
+    }
+
+    var rows = tableClone.querySelectorAll('tbody tr');
+    rows.forEach(row => {
+        var editCell = row.querySelector('td:nth-child(2)');
+        if (editCell) {
+            row.removeChild(editCell);
+        }
+    });
+
+    var tableString = doc.autoTableHtmlToJson(tableClone);
     doc.setFontSize(fontSize);
     var margin = {
         top: 30,
@@ -13,6 +29,7 @@ document.getElementById('btnDownload').addEventListener('click', function () {
         bottom: 30,
         left: 30
     };
+
     function drawTablePart(startRowIndex, endRowIndex) {
         var dataPart = tableString.data.slice(startRowIndex, endRowIndex);
         doc.autoTable({
@@ -25,17 +42,24 @@ document.getElementById('btnDownload').addEventListener('click', function () {
             margin: margin
         });
     }
+
+    // Cálculo do tamanho da tabela
     var tableWidth = table.offsetWidth;
     if (tableWidth > maxTableWidth) {
         var rowCountPerPage = 25;
         var rowCount = tableString.data.length;
-        var currentPage = 1;
         var startRowIndex = 0;
         var endRowIndex = rowCountPerPage;
+
+        // Gerar a primeira página sem addPage()
+        drawTablePart(startRowIndex, endRowIndex);
+        startRowIndex = endRowIndex;
+        endRowIndex = Math.min(startRowIndex + rowCountPerPage, rowCount);
+
+        // Loop para adicionar as páginas seguintes
         while (startRowIndex < rowCount) {
             doc.addPage();
             drawTablePart(startRowIndex, endRowIndex);
-            currentPage++;
             startRowIndex = endRowIndex;
             endRowIndex = Math.min(startRowIndex + rowCountPerPage, rowCount);
         }
@@ -50,5 +74,6 @@ document.getElementById('btnDownload').addEventListener('click', function () {
         });
     }
 
+    // Salvar o PDF
     doc.save('relatorio.pdf');
 });
