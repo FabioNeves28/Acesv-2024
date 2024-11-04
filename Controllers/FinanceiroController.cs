3311633ba163053b7df053a3f98e0a74ef3092ed
Fabio <fabioalvesneves@gmail.com>
2024-11-04 11:53:46 -0300

    ajustes na tela do financeiro

--- Controllers/FinanceiroController.cs
+++ Controllers/FinanceiroController.cs
@@ -58,7 +58,7 @@ namespace Acesvv.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Mes,Saldo_Mes,Arrecadacao_Mensalidade_Atrasada,Arrecadacao_Mensalidade_Antecipadas,Total_Entradas,Vencimento,Contabilidade,Tarifa_Bancaria,Apolice_Seguro,Advogada,Renovacao_Assinatura,Taxas_Bancarias,Taxa_Internet,Total_Gastos,Total_Liquido")] Financeiro financeiro)
+        public async Task<IActionResult> Create([Bind("ID,Mes,Saldo_Mes,Arrecadacao_Mensalidade_Atrasada,Arrecadacao_Mensalidade_Antecipadas,Total_Entradas,Contabilidade,Tarifa_Bancaria,Apolice_Seguro,Advogada,Renovacao_Assinatura,Taxas_Bancarias,Taxa_Internet,Total_Gastos,Total_Liquido")] Financeiro financeiro)
         {
             if (ModelState.IsValid)
             {
@@ -93,7 +93,7 @@ namespace Acesvv.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Mes,Saldo_Mes,Arrecadacao_Mensalidade_Atrasada,Arrecadacao_Mensalidade_Antecipadas,Total_Entradas,Vencimento,Contabilidade,Tarifa_Bancaria,Apolice_Seguro,Advogada,Renovacao_Assinatura,Taxas_Bancarias,Taxa_Internet,Total_Gastos,Total_Liquido")] Financeiro financeiro)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Mes,Saldo_Mes,Arrecadacao_Mensalidade_Atrasada,Arrecadacao_Mensalidade_Antecipadas,Total_Entradas,Contabilidade,Tarifa_Bancaria,Apolice_Seguro,Advogada,Renovacao_Assinatura,Taxas_Bancarias,Taxa_Internet,Total_Gastos,Total_Liquido")] Financeiro financeiro)
         {
             if (id != financeiro.ID)
             {
